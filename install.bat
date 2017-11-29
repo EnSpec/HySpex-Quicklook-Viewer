21117cc22921f9b9701361220c6634ffb415994b
Matthew Westphall <westphall@russell.wisc.edu>
2017-11-29 16:27:12 -0600

    updated installer to generate shortcut rather than copying

--- install.bat
+++ install.bat
@@ -1,2 +1,16 @@
-pip install -r requirements.txt
+::Script to generate shortcut
+
+set SCRIPT="%TEMP%\%RANDOM%-%RANDOM%-%RANDOM%-%RANDOM%.vbs"
+
+echo Set oWS = WScript.CreateObject("WScript.Shell") >> %SCRIPT%
+echo sLinkFile = "quicklooks.lnk" >> %SCRIPT%
+echo Set oLink = oWS.CreateShortcut(sLinkFile) >> %SCRIPT%
+echo oLink.TargetPath = "quicklooks.pyw" >> %SCRIPT%
+echo oLink.Save >> %SCRIPT%
+
+cscript /nologo %SCRIPT%
+del %SCRIPT%
 copy quicklooks.lnk %userprofile%\Desktop
+pip install -r requirements.txt
+
+
